f8b30878b938a14abb57909a15537a95611b284d
Emre 'imribiy' Akman <95129355+sirimribiy@users.noreply.github.com>
2022-03-19 19:15:43 +0300

    Update ShellExperienceHost.bat

--- ShellExperienceHost.bat
+++ ShellExperienceHost.bat
@@ -1,6 +1,8 @@
 @echo off
 title ShellExperienceHost Toggle Script
 
+echo  This script will also break Task Scheduler, so dont use it if you need Task Scheduler!
+echo.
 echo.	Press [D] to Disable SEH
 echo.	Press [E] to Enable SEH (XOS Default)
 echo.
@@ -12,6 +14,9 @@ if /i %c% equ E goto :enable
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\ahcache" /v "Start" /t REG_DWORD /d "4" /f
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\systemeventsbroker" /v "Start" /t REG_DWORD /d "4" /f
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\samss" /v "Start" /t REG_DWORD /d "4" /f
+PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "Start" /t REG_DWORD /d "4" /f
+PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\timebrokersvc" /v "Start" /t REG_DWORD /d "4" /f
+PowerRun.exe /SW:0 Reg.exe delete "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "ErrorControl" /f
 taskkill /f /im explorer.exe
 taskkill /f /im runtimebroker.exe
 cd C:\Windows\System32
@@ -28,6 +33,9 @@ exit
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\ahcache" /v "Start" /t REG_DWORD /d "1" /f
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\systemeventsbroker" /v "Start" /t REG_DWORD /d "2" /f
 PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\samss" /v "Start" /t REG_DWORD /d "2" /f
+PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "3" /f
+PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\schedule" /v "Start" /t REG_DWORD /d "2" /f
+PowerRun.exe /SW:0 "Reg.exe" add "HKLM\SYSTEM\CurrentControlSet\Services\schedule" /v "errorcontrol" /t REG_DWORD /d "1" /f
 taskkill /f /im explorer.exe
 cd C:\Windows\System32
 takeown /f "runtimebroker.old"
